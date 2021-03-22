b4ca037dd0021a658b9f1a0a72795168afed88cc
Pranav Prakasan <pranav.prakasan2019@gmail.com>
2021-03-22 14:56:21 +0530

    Login Page UI

---
+++ app/src/main/java/com/example/findest/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.findest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
+
+/*
+
+yellow : #FFD351
+light blue : #85CFEA
+dull blue : #395185
+dark blue : #1976D2
+blue : #28A6DE
+green : #4CAF50
+black : #231F20
+grey : #405866
+white : #F7F7F7
+bg blue : #11223A
+ */
