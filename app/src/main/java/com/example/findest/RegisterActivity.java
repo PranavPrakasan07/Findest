1b64e88659ebc5d7916c99effd3d979798cb5237
Pranav Prakasan <pranav.prakasan2019@gmail.com>
2021-03-22 15:38:10 +0530

    Register Page UI

--- app/src/main/java/com/example/findest/RegisterActivity.java
+++ app/src/main/java/com/example/findest/RegisterActivity.java
@@ -5,10 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 
-public class LoginActivity extends AppCompatActivity {
+public class RegisterActivity extends AppCompatActivity {
 
     //Facing rendering issue with Button, so using a TextView
     TextView register_button;
@@ -16,14 +15,14 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_register);
 
         register_button = findViewById(R.id.register_button);
 
         register_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(getApplicationContext(), RegisterActivity.class));
+                startActivity(new Intent(getApplicationContext(), LoginActivity.class));
             }
         });
     }
