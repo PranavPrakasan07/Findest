e4cec485267b806d861b19f7f85ab0bee0263ea0
Pranav Prakasan <pranav.prakasan2019@gmail.com>
2021-03-22 22:28:55 +0530

    Work Preferences UI

---
+++ app/src/main/java/com/example/findest/IntroductionToRecuiter.java
@@ -0,0 +1,14 @@
+package com.example.findest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class IntroductionToRecuiter extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_introduction_to_recuiter);
+    }
+}
--- app/src/main/java/com/example/findest/WorkPreferencesActivity.java
+++ app/src/main/java/com/example/findest/WorkPreferencesActivity.java
@@ -1,27 +1,29 @@
 package com.example.findest;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
-public class KeySkillsActivity extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
+
+public class WorkPreferencesActivity extends AppCompatActivity {
+
     TextView next_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_key_skills);
+        setContentView(R.layout.activity_work_preferences);
 
         next_button = findViewById(R.id.next_button);
 
         next_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(getApplicationContext(), WorkPreferencesActivity.class));
+                startActivity(new Intent(getApplicationContext(), IntroductionToRecuiter.class));
             }
         });
+
     }
 }
